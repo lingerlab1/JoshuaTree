6a6fb04f0f42bae57b592ae6cb4832fb60dec6ff
Yingyu Fu <yingyu.fu@outlook.com>
2022-02-09 14:29:21 -0500

    upgrade the nvim config

--- python/monotonic.py
+++ python/monotonic.py
@@ -7,13 +7,13 @@ s1, s2  = [], []
 
 for i, num in enumerate(A):
     count = 1
-    while s1 and s1[-1][0] > num: 
+    while s1 and s1[-1][0] > num:
         count += s1.pop()[1]
     left_min[i] = count
     s1.append([num, count])
 
     count = 1
-    while s2 and s2[-1][0] < num: 
+    while s2 and s2[-1][0] < num:
         count += s2.pop()[1]
     left_max[i] = count
     s2.append([num, count])
@@ -21,17 +21,19 @@ for i, num in enumerate(A):
 s1, s2  = [], []
 for i, num in list(enumerate(A))[::-1]:
     count = 1
-    while s1 and s1[-1][0] > num: 
+    while s1 and s1[-1][0] > num:
         count += s1.pop()[1]
     right_min[i] = count
     s1.append([num, count])
 
     count = 1
-    while s2 and s2[-1][0] < num: 
+    while s2 and s2[-1][0] < num:
         count += s2.pop()[1]
     right_max[i] = count
     s2.append([num, count])
 
+c = [1,2,33,34,3434,3434,3434343,
+        3434,434]
 
 print(sum(a * l * r for a, l, r in zip(A, left_min, right_min)))
 print(sum(a * l * r for a, l, r in zip(A, left_max, right_max)))
