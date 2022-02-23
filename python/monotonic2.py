ac146c50f6d545dfcd29e279270f789f6f67724a
Yingyu Fu <yingyu.fu@outlook.com>
2022-02-23 13:38:31 -0500

    add leetcode files

--- python/monotonic2.py
+++ python/monotonic2.py
@@ -1,10 +1,10 @@
 A = [2, 1, 3]
 
 n, mod = len(A), 10**9 + 7
-left_min, right_min= [0] * n, [0] * n
-left_sum, right_sum= [0] * n, [0] * n
+left_min, right_min = [0] * n, [0] * n
+left_sum, right_sum = [0] * n, [0] * n
 
-s1= []
+s1 = []
 
 for i, num in enumerate(A):
     count = 1
@@ -12,7 +12,7 @@ for i, num in enumerate(A):
     while s1 and s1[-1][0] > num:
         n, c = s1.pop()
         count += c
-        s += n 
+        s += n
     left_min[i] = count
     left_sum[i] = s
     s1.append([num, count])
@@ -24,11 +24,11 @@ for i, num in list(enumerate(A))[::-1]:
     while s1 and s1[-1][0] > num:
         n, c = s1.pop()
         count += c
-        s += n 
+        s += n
     right_min[i] = count
     right_sum[i] = s
     s1.append([num, count])
 
 
-
-print(sum(l * r * (ls + rs - a) for l, r, ls, rs, a in zip(left_min, right_min, left_sum, right_sum, A)))
+print(sum(l * r * (ls + rs - a)
+      for l, r, ls, rs, a in zip(left_min, right_min, left_sum, right_sum, A)))
